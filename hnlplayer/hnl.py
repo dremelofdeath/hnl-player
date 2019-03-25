ecff08a1043ac66b60c4c2cbb550deb17b2df5b0
Zachary Murray <dremelofdeath@gmail.com>
2019-03-24 21:30:46 -0700

    Lots of titleformat and column configuration stuff.

--- hnlplayer/hnl.py
+++ hnlplayer/hnl.py
@@ -2,22 +2,42 @@
 # -*- coding: utf-8 -*-
 # vim:ts=2:sw=2:et:ai
 
+import copy
 import mutagen
 import os.path
 import sys
 
 from euphonogenizer import titleformat as tf
 
-from typing import List
+from typing import Dict, List
 
 from PyQt5 import QtCore
-from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex, QUrl
-from PyQt5.QtGui import QPalette
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
+
+class HNLInternalError(Exception):
+  pass
 
 
 disabledPalette = QPalette()
 disabledPalette.setColor(QPalette.Base, Qt.lightGray)
+
+
+# From Foobar-like keys to Mutagen keys
+mutagen_redirect_keys: Dict[str, str] = {
+    'album artist': 'albumartist',
+    'publisher': 'organization',
+    'totaldiscs': 'disctotal',
+    'totaltracks': 'tracktotal',
+}
+
+
+def marshal_key(key: str) -> str:
+  if key not in mutagen_redirect_keys:
+    return key
+  return mutagen_redirect_keys[key]
 
 
 class MutagenFileProxy:
@@ -33,13 +53,13 @@ class MutagenFileProxy:
     return getattr(self.mutagen_file, attr)
 
   def get(self, key, default=None):
-    item = self.mutagen_file.get(key, default)
+    item = self.mutagen_file.get(marshal_key(key), default)
     if isinstance(item, list):
       return item[0]
     return item
 
   def __getitem__(self, key):
-    item = self.mutagen_file[key]
+    item = self.mutagen_file[marshal_key(key)]
     if isinstance(item, list):
       return item[0]
     return item
@@ -58,30 +78,65 @@ playlistAllowedMimeTypes = [
 
 
 class PlaylistColumn:
-  __slots__ = 'name', 'defaultWidth', 'fmt', 'tfc'
+  __slots__ = 'name', 'defaultWidth', '_fmt', '_tfc'
 
-  def __init__(self, name, defaultWidth, fmt):
+  def __init__(self, name='', defaultWidth=180, fmt=''):
     self.name = name
     self.defaultWidth = defaultWidth
     self.fmt = fmt
-    self.tfc = tf.compile(fmt)
+
+  @property
+  def fmt(self):
+    return self._fmt
+
+  @fmt.setter
+  def fmt(self, fmt: str):
+    self._fmt = fmt
+    self._tfc = tf.compile(fmt)
+
+  def format(self, track):
+    return self._tfc(track)
+
+  def __copy__(self):
+    return PlaylistColumn(self.name, self.defaultWidth, self.fmt)
+
+  def __deepcopy__(self, memo=None):
+    return PlaylistColumn(self.name, self.defaultWidth, self.fmt)
 
 
 class PlaylistModel(QAbstractTableModel):
-  def __init__(self, columns, tracks, parent=None):
+  def __init__(
+      self,
+      columns: List[PlaylistColumn],
+      tracks: List[MutagenFileProxy],
+      parent=None):
     super().__init__(parent)
-    self.columns = columns
-    self.tracks = tracks
+    self._columns: List[PlaylistColumn] = columns
+    self._tracks: List[MutagenFileProxy] = tracks
+
+  @property
+  def columns(self):
+    return self._columns
+
+  @columns.setter
+  def columns(self, columns: List[PlaylistColumn]) -> None:
+    self.layoutAboutToBeChanged.emit()
+    self._columns = columns
+    self.layoutChanged.emit()
+
+  def getTrack(self, index: QModelIndex) -> MutagenFileProxy:
+    if index.isValid():
+      return self._tracks[index.row()]
 
   def rowCount(self, parent=QModelIndex()):
-    return len(self.tracks)
+    return len(self._tracks)
 
   def columnCount(self, parent=QModelIndex()):
     return len(self.columns)
 
   def data(self, index, role):
     if index.isValid() and role == Qt.DisplayRole:
-      return self.columns[index.column()].tfc(self.tracks[index.row()])
+      return self.columns[index.column()].format(self._tracks[index.row()])
 
   def headerData(self, section, orientation, role):
     if role == Qt.DisplayRole and orientation == Qt.Horizontal:
@@ -89,9 +144,9 @@ class PlaylistModel(QAbstractTableModel):
 
   def insertTrack(self, row, track):
     if row < 0:
-      row = len(self.tracks)
+      row = len(self._tracks)
     self.layoutAboutToBeChanged.emit()
-    self.tracks.insert(row, track)
+    self._tracks.insert(row, track)
     self.layoutChanged.emit()
 
   def insertTrackPath(self, row, track_path):
@@ -107,9 +162,9 @@ class PlaylistModel(QAbstractTableModel):
         sourceParent, sourceRow, sourceRow,
         destinationParent, destinationChild)
     if sourceRow > destinationChild:
-      self.tracks.insert(destinationChild, self.tracks.pop(sourceRow))
+      self._tracks.insert(destinationChild, self._tracks.pop(sourceRow))
     elif sourceRow < destinationChild:
-      self.tracks.insert(destinationChild - 1, self.tracks.pop(sourceRow))
+      self._tracks.insert(destinationChild - 1, self._tracks.pop(sourceRow))
     self.endMoveRows()
     return True
 
@@ -120,27 +175,27 @@ class PlaylistModel(QAbstractTableModel):
         sourceParent, sourceRow, sourceRow + count - 1,
         destinationParent, destinationChild)
     if sourceRow > destinationChild:
-      self.tracks = (self.tracks[:destinationChild]
-              + self.tracks[destinationChild:sourceRow]
-              + self.tracks[sourceRow:sourceRow+count]
-              + self.tracks[sourceRow+count:])
+      self._tracks = (self._tracks[:destinationChild]
+              + self._tracks[destinationChild:sourceRow]
+              + self._tracks[sourceRow:sourceRow+count]
+              + self._tracks[sourceRow+count:])
     else:
-      self.tracks = (self.tracks[:sourceRow]
-              + self.tracks[sourceRow:sourceRow+count]
-              + self.tracks[sourceRow+count:destinationChild]
-              + self.tracks[destinationChild:])
+      self._tracks = (self._tracks[:sourceRow]
+              + self._tracks[sourceRow:sourceRow+count]
+              + self._tracks[sourceRow+count:destinationChild]
+              + self._tracks[destinationChild:])
     self.endMoveRows()
     return True
 
   def removeRow(self, row, parent):
     self.beginRemoveRows(parent, row, row)
-    del self.tracks[row]
+    del self._tracks[row]
     self.endRemoveRows()
     return True
 
   def removeRows(self, row, count, parent):
     self.beginRemoveRows(parent, row, row + count - 1)
-    del self.tracks[row:row + count - 1]
+    del self._tracks[row:row + count - 1]
     self.endRemoveRows()
     return True
 
@@ -194,6 +249,7 @@ def set_basic_table_styles(
   hh.setHighlightSections(False)
   hh.setDefaultAlignment(Qt.AlignLeft)
   vh.setSectionResizeMode(QHeaderView.ResizeToContents)
+  vh.setHighlightSections(False)
 
 
 class ColumnConfigurationModel(QAbstractTableModel):
@@ -239,50 +295,91 @@ class ColumnConfigurationTableView(QTableView):
 
     set_basic_table_styles(self, hh, vh)
 
+    self.setSelectionMode(QAbstractItemView.SingleSelection)
+
     hh.setStretchLastSection(True)
 
 
-class ConfigureColumnsDialog(QDialog):
-  def __init__(self, columns, parent):
+class ColumnConfigurationLayout(QGridLayout):
+  def __init__(
+      self,
+      columns: List[PlaylistColumn],
+      mainWindow: QMainWindow,
+      parent: QWidget):
     super().__init__(parent)
+
+    columns = copy.deepcopy(columns)
     self.columns = columns
+    self.mainWindow = mainWindow
+    self.parent = parent
 
-    self.setModal(False)
-    self.setWindowTitle('Configure Columns')
-    self.resize(640, 480)
+    self.columnsTableView = ColumnConfigurationTableView(columns, parent)
+    self.addWidget(self.columnsTableView, 1, 1, 2, 4)
 
-    self.layout = QGridLayout()
+    self.columnNameLabel = QLabel('&Name:', parent)
+    self.columnNameTextBox = QLineEdit(parent)
+    self.columnNameLabel.setBuddy(self.columnNameTextBox)
+    self.columnWidthLabel = QLabel('&Width:', parent)
+    self.columnWidthTextBox = QLineEdit(parent)
+    self.columnWidthLabel.setBuddy(self.columnWidthTextBox)
+    self.columnFormatLabel = QLabel('&Format:', parent)
+    self.columnFormatTextBox = QLineEdit(parent)
+    self.columnFormatLabel.setBuddy(self.columnFormatTextBox)
 
-    self.columnsTableView = ColumnConfigurationTableView(columns, self)
+    widthValidator = QIntValidator(self.columnWidthTextBox)
+    widthLocale = QLocale()
+    widthLocale.setNumberOptions(QLocale.NumberOption.RejectGroupSeparator)
+    widthValidator.setLocale(widthLocale)
+    self.columnWidthTextBox.setValidator(widthValidator)
 
-    selectionModel = self.columnsTableView.selectionModel()
-    selectionModel.currentRowChanged.connect(self.onCurrentRowChanged)
+    self.addWidget(self.columnNameLabel, 3, 1)
+    self.addWidget(self.columnNameTextBox, 3, 2)
+    self.addWidget(self.columnWidthLabel, 3, 3)
+    self.addWidget(self.columnWidthTextBox, 3, 4)
+    self.addWidget(self.columnFormatLabel, 4, 1, 1, 1)
+    self.addWidget(self.columnFormatTextBox, 4, 2, 1, 3)
 
-    self.layout.addWidget(self.columnsTableView, 1, 1, 2, 4)
+    self.columnNameTextBox.textChanged.connect(self.onColumnNameChanged)
+    self.columnWidthTextBox.textChanged.connect(self.onColumnWidthChanged)
+    self.columnFormatTextBox.textChanged.connect(self.onColumnFormatChanged)
+    self.columnNameTextBox.textEdited.connect(self.onColumnNameEdited)
+    self.columnWidthTextBox.textEdited.connect(self.onColumnWidthEdited)
+    self.columnFormatTextBox.textEdited.connect(self.onColumnFormatEdited)
 
-    self.columnNameLabel = QLabel('Name:', self)
-    self.columnNameTextBox = QLineEdit(self)
-    self.columnWidthLabel = QLabel('Width:', self)
-    self.columnWidthTextBox = QLineEdit(self)
-    self.columnFormatLabel = QLabel('Format:', self)
-    self.columnFormatTextBox = QLineEdit(self)
-
-    self.layout.addWidget(self.columnNameLabel, 3, 1)
-    self.layout.addWidget(self.columnNameTextBox, 3, 2)
-    self.layout.addWidget(self.columnWidthLabel, 3, 3)
-    self.layout.addWidget(self.columnWidthTextBox, 3, 4)
-    self.layout.addWidget(self.columnFormatLabel, 4, 1, 1, 1)
-    self.layout.addWidget(self.columnFormatTextBox, 4, 2, 1, 3)
-
-    self.previewLabel = QLabel('Preview:', self)
-    self.previewTextBox = QLineEdit(self)
+    self.previewLabel = QLabel('Preview:', parent)
+    self.previewTextBox = QLineEdit(parent)
+    self.previewLabel.setBuddy(self.previewTextBox)
     self.previewTextBox.setReadOnly(True)
     self.previewTextBox.setPalette(disabledPalette)
 
-    self.layout.addWidget(self.previewLabel, 5, 1, 1, 1)
-    self.layout.addWidget(self.previewTextBox, 5, 2, 1, 3)
+    self.addWidget(self.previewLabel, 5, 1, 1, 1)
+    self.addWidget(self.previewTextBox, 5, 2, 1, 3)
 
-    self.setLayout(self.layout)
+    self.addNewButton = QPushButton('Add New', parent)
+    self.deleteButton = QPushButton('Delete', parent)
+    self.resetButton = QPushButton('Reset', parent)
+
+    if isinstance(parent, QDialog):
+      self.saveButton = QPushButton('OK', parent)
+    else:
+      self.saveButton = QPushButton('Save', parent)
+
+    self.addNewButton.clicked.connect(self.onAddNewButtonClicked)
+    self.deleteButton.clicked.connect(self.onDeleteButtonClicked)
+    self.resetButton.clicked.connect(self.onResetButtonClicked)
+    self.saveButton.clicked.connect(self.onSaveButtonClicked)
+
+    self.buttonLayout = QHBoxLayout()
+    self.buttonLayout.addWidget(self.addNewButton)
+    self.buttonLayout.addWidget(self.deleteButton)
+    self.buttonLayout.addWidget(self.resetButton)
+    self.buttonLayout.addStretch()
+    self.buttonLayout.addWidget(self.saveButton)
+
+    self.addLayout(self.buttonLayout, 6, 1, 1, 4)
+
+    self.selectionModel = self.columnsTableView.selectionModel()
+    self.selectionModel.currentRowChanged.connect(self.onCurrentRowChanged)
 
   def onCurrentRowChanged(self, current, previous):
     col = self.columns[current.row()]
@@ -290,19 +387,130 @@ class ConfigureColumnsDialog(QDialog):
     self.columnWidthTextBox.setText(str(col.defaultWidth))
     self.columnFormatTextBox.setText(col.fmt)
 
+  def onColumnNameChanged(self, text):
+    pass
+
+  def onColumnNameEdited(self, text):
+    self.onColumnNameChanged(text)
+    self.handleColumnEdit(0, text)
+
+  def onColumnWidthChanged(self, text):
+    pass
+
+  def onColumnWidthEdited(self, text):
+    self.onColumnWidthChanged(text)
+    self.handleColumnEdit(1, text)
+
+  def onColumnFormatChanged(self, text):
+    self.previewTextBox.setText(
+        str(tf.format(text, self.mainWindow.playlistView.currentTrack)))
+
+  def onColumnFormatEdited(self, text):
+    self.onColumnFormatChanged(text)
+    self.handleColumnEdit(2, text)
+
+  def handleColumnEdit(self, col, text):
+    index = self.selectionModel.currentIndex()
+    if index.isValid():
+      row = index.row()
+      if col == 0:
+        self.columns[row].name = text
+      elif col == 1:
+        self.columns[row].defaultWidth = int(text) if text else 0
+      elif col == 2:
+        self.columns[row].fmt = text
+      else:
+        raise IndexError(f"Can't edit nonexistent column {col}.")
+      index = self.columnsTableView.columnsModel.createIndex(row, col)
+      self.columnsTableView.dataChanged(index, index)
+
+  def onAddNewButtonClicked(self, checked):
+    if len(self.columns) == 1:
+      self.deleteButton.setEnabled(True)
+
+    cm = self.columnsTableView.columnsModel
+
+    cm.layoutAboutToBeChanged.emit()
+    self.columns.append(PlaylistColumn())
+    cm.layoutChanged.emit()
+
+    newRowIndex = cm.index(len(self.columns) - 1, 0)
+    sm = self.selectionModel
+
+    sm.setCurrentIndex(newRowIndex, QItemSelectionModel.NoUpdate)
+    sm.select(
+        newRowIndex,
+        QItemSelectionModel.ClearAndSelect | QItemSelectionModel.Rows)
+
+    self.columnNameTextBox.setFocus()
+
+  def onDeleteButtonClicked(self, checked):
+    if len(self.columns) <= 1:
+      raise HNLInternalError('Attempted to delete the only remaining column.')
+
+    index = self.selectionModel.currentIndex()
+    if index.isValid():
+      row = index.row()
+      cm = self.columnsTableView.columnsModel
+
+      cm.layoutAboutToBeChanged.emit()
+      del self.columns[row]
+      cm.layoutChanged.emit()
+
+      if row != len(self.columns):
+        # Force the update because changing the index to itself does nothing
+        self.onCurrentRowChanged(cm.index(row, 0), None)
+        return
+
+      newRowIndex = cm.index(row - 1, 0)
+      sm = self.selectionModel
+
+      sm.setCurrentIndex(newRowIndex, QItemSelectionModel.NoUpdate)
+      sm.select(
+          newRowIndex,
+          QItemSelectionModel.ClearAndSelect | QItemSelectionModel.Rows)
+
+      if len(self.columns) <= 1:
+        self.deleteButton.setEnabled(False)
+
+  def onResetButtonClicked(self, checked):
+    # TODO: Dialog asking if the user wants to reset to the playlist or defaults
+    self.deleteButton.setEnabled(True)
+
+  def onSaveButtonClicked(self, checked):
+    self.mainWindow.playlistView.columns = self.columns
+    if isinstance(self.parent, QDialog):
+      self.parent.done(QDialog.Accepted)
+
+
+class ConfigureColumnsDialog(QDialog):
+  def __init__(self, columns, mainWindow, parent):
+    super().__init__(parent)
+    self.columns = columns
+    self.mainWindow = mainWindow
+
+    self.setModal(False)
+    self.setWindowTitle('Configure Columns')
+    self.resize(640, 480)
+
+    self.contents = ColumnConfigurationLayout(columns, mainWindow, self)
+
 
 class PlaylistTableView(QTableView):
   def __init__(self, parent=None):
     super().__init__(parent)
-    self.columns = [
+
+    self.mainWindow = parent
+
+    self.playlist = PlaylistModel([
         PlaylistColumn('Track', 30, '%track%'),
         PlaylistColumn('Title', 180, '%title%'),
         PlaylistColumn('Artist', 180, '%artist%'),
         PlaylistColumn('Album', 180, '%album%'),
-      ]
+      ], [])
 
-    self.playlist = PlaylistModel(self.columns, [])
     self.setModel(self.playlist)
+    self.nowPlaying: int = -1
 
     hh = self.horizontalHeader()
     vh = self.verticalHeader()
@@ -329,9 +537,32 @@ class PlaylistTableView(QTableView):
     for i, each in enumerate(self.playlist.columns):
       self.setColumnWidth(i, each.defaultWidth)
 
+    self.doubleClicked.connect(self.onDoubleClicked)
+
+  @property
+  def columns(self):
+    return self.playlist.columns
+
+  @columns.setter
+  def columns(self, columns: List[PlaylistColumn]) -> None:
+    self.playlist.columns = columns
+
+  @property
+  def currentTrack(self):
+    if self.nowPlaying >= 0:
+      return self.playlist._tracks[self.nowPlaying]
+
+  def onDoubleClicked(self, index: QModelIndex) -> None:
+    if index.isValid():
+      self.play(index)
+
+  def play(self, trackIndex: QModelIndex) -> None:
+    self.nowPlaying = trackIndex.row()
+    track = self.playlist.getTrack(trackIndex)
+    self.mainWindow.updateTitleForPlayingTrack(track)
+
   def enactColumnContextMenu(self, pos):
     self.columnContextMenu.exec_(self.mapToGlobal(pos))
-
 
   def dropEvent(self, event):
     if not event.isAccepted() and event.source() == self:
@@ -422,7 +653,7 @@ class PlayerViewMenuContainer:
 
   def onConfigureColumns(self):
     configureColumnsDialog = ConfigureColumnsDialog(
-        self.mainWindow.playlistView.columns, self.mainWindow)
+        self.mainWindow.playlistView.columns, self.mainWindow, self.mainWindow)
     configureColumnsDialog.show()
 
 
@@ -461,12 +692,6 @@ class PlayerMenuBarContainer:
         mainWindow, menuBar.addMenu('Help'))
 
 
-def fetch_meta(obj, field):
-  if type(obj[field]) is list:
-    return obj[field][0]
-  return obj[field]
-
-
 class PlayerMainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
@@ -483,19 +708,26 @@ class PlayerMainWindow(QMainWindow):
     self.centralWidget.setLayout(self.layout)
     self.setCentralWidget(self.centralWidget)
 
+    self.playingTitleFormat = tf.compile(
+      "HNL - %title% - %artist% - %album% '('#%track% / %totaltracks%')'")
+
   def buildPlayerControlsToolbar(self):
+    style = self.style()
     toolbar = self.addToolBar('Controls')
     stop = toolbar.addAction(
-        self.style().standardIcon(QStyle.SP_MediaStop), '', lambda: None)
+        style.standardIcon(QStyle.SP_MediaStop), '', lambda: None)
     play = toolbar.addAction(
-        self.style().standardIcon(QStyle.SP_MediaPlay), '', lambda: None)
+        style.standardIcon(QStyle.SP_MediaPlay), '', lambda: None)
     pause = toolbar.addAction(
-        self.style().standardIcon(QStyle.SP_MediaPause), '', lambda: None)
+        style.standardIcon(QStyle.SP_MediaPause), '', lambda: None)
     back = toolbar.addAction(
-        self.style().standardIcon(QStyle.SP_MediaSkipBackward), '', lambda: None)
+        style.standardIcon(QStyle.SP_MediaSkipBackward), '', lambda: None)
     forward = toolbar.addAction(
-        self.style().standardIcon(QStyle.SP_MediaSkipForward), '', lambda: None)
+        style.standardIcon(QStyle.SP_MediaSkipForward), '', lambda: None)
     return toolbar
+
+  def updateTitleForPlayingTrack(self, track: MutagenFileProxy) -> None:
+    self.setWindowTitle(self.playingTitleFormat(track))
 
 
 def main():
